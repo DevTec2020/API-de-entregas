5817a839ff38173220e62aa5011480e63173a2af
DevTec2020 <leonardo.silva.inf@gmail.com>
2025-08-14 22:08:25 -0300

    feat: Melhorando os logs de entrega

--- src/routes/delivery-logs-routs.ts
+++ src/routes/delivery-logs-routs.ts
@@ -9,11 +9,7 @@ const deliveryLogsRoutes = Router()
 
 const deliveryLogsController = new DeliveryLogsController()
 
-deliveryLogsRoutes.post(
-    "/", 
-    ensureAuthenticated,
-    verifyUserAuthorization(["sale"]),
-    deliveryLogsController.create
-)
+deliveryLogsRoutes.post("/", ensureAuthenticated, verifyUserAuthorization(["sale"]), deliveryLogsController.create)
+deliveryLogsRoutes.get("/:delivery_id/show", ensureAuthenticated, verifyUserAuthorization(["sale", "customer"]), deliveryLogsController.show)
 
 export { deliveryLogsRoutes }
