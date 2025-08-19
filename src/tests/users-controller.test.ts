60278a901838ecb6538ce24e8a90177dd7c72027
DevTec2020 <leonardo.silva.inf@gmail.com>
2025-08-18 21:08:06 -0300

    Test: Adicionando testes de castro

--- src/tests/users-controller.test.ts
+++ src/tests/users-controller.test.ts
@@ -24,4 +24,26 @@ describe("UserController", () => {
         user_id = response.body.id
 
     })
+
+    it("Should throw an error if user with same email already exists", async () => {
+        const response = await request(app).post("/users").send({
+            name: "Duplicate User",
+            email: "testuser@example.com",
+            password: "password123",
+        })
+
+        expect(response.status).toBe(400)
+        expect(response.body.message).toBe("Este e-mail já está em uso")
+    })
+
+    it("should throw an error if user with same email already exists", async () => {
+        const response = await request(app).post("/users").send({
+            name: "Test User",
+            email: "invalid-email",
+            password: "password123",
+        })
+
+        expect(response.status).toBe(400)
+        expect(response.body.message).toBe("Validation error")
+    })
 })
